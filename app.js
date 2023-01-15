c83f3d5d7d334c4624d0c7afb585b477b20e1422
Yoldfred Suarez <yoldfred.suarez@gmail.com>
2023-01-15 11:53:46 -0400

    triangulo en su forma bace

---
+++ app.js
@@ -0,0 +1,37 @@
+let input = Number(prompt('ingresar numero de lineas'));
+let output = '#';
+let numero = 1;
+
+/*for(i=1;i<=input;i++){
+    console.log(output ,'<br>');
+    output += '#' 
+}*/
+
+/*do{
+    console.log(output ,'<br>')
+    output +='#'
+    numero++
+}while(numero <= input);*/
+
+while (numero <= input) {
+    document.write(output ,'<br>');
+    output += '#';
+    numero++
+}
+
+//     -- Algoritmo --
+
+//  ingresar Numero en "input"
+
+//      asignar a "output" el valor '#'
+
+//      asignar a "numero" el valor 1
+
+/*      [iniciar loop]
+        - imprimir output;
+        - agregar '#' a "outpur"
+        -agregar 1 a "numero"
+        [fin del loop]
+*/
+
+//      -- fin del algoritmo -- 
